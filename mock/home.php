1c909befe809159160bca54c89d71005721ef459
Rajnesh Prajapati <raznesh.prajapati@gmail.com>
2022-09-25 11:57:57 +0545

    Test for cross origin issue.

--- mock/home.php
+++ mock/home.php
@@ -1,6 +1,7 @@
 <?php
 
-header('Access-Control-Allow-Origin: *; Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=utf-8');
 
 echo json_encode(
   [
